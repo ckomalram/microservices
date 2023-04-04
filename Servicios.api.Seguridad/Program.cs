b18b347343d9553f765620cbfed4993628ff3996
ckomalram <glaw14@gmail.com>
2023-04-04 11:40:49 -0500

    ++ inyectando cnn sql server  + ejecutando 1ra migracion

--- Servicios.api.Seguridad/Program.cs
+++ Servicios.api.Seguridad/Program.cs
@@ -1,3 +1,5 @@
+using Servicios.api.Seguridad.Core.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Agregar EF
+builder.Services.AddSqlServer<SeguridadContext>(builder.Configuration.GetConnectionString("cnSeguridadDb"));
 
 var app = builder.Build();
 
